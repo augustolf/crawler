cf9cb9ad80025ed1529108cfc9f61c571d270004
Luiz Augusto <augustolf@gmail.com>
2015-11-07 00:31:17 -0300

    Crawlers work

--- crawler.js
+++ crawler.js
@@ -1,6 +1,54 @@
-var crawler = {
-    name: "John",
-    x: 0,
-    y: 0,
-    color: "blue"
+var crawlers = {
+    size: 10,
+    list: [],
+    init: function(areaWidth, areaHeigh, amount) {
+      for (var i = 0; i < amount; i++) {
+        this.list[i] = {
+          name: "c" + 1,
+          x: Math.floor(Math.random() * areaWidth),
+          y: Math.floor(Math.random() * areaHeigh),
+          life: 5
+        };
+      }
+    },
+    draw: function(ctx) {
+      for (var i = 0; i < this.list.length; i++) {
+        ctx.fillStyle = "rgb(255, 0, 0)";
+        ctx.fillRect(this.list[i].x * this.size, this.list[i].y * this.size, this.size, this.size);
+        console.log("x: " + (this.list[i].x* this.size) + " y: " + (this.list[i].y* this.size));
+      }
+    },
+    action: function(mat) {
+      for (var i = 0; i < this.list.length; i++) {
+        console.log("grass[" + this.list[i].x + "][" + this.list[i].y + "]:" + mat[this.list[i].x][this.list[i].y]);
+        if (mat[this.list[i].x][this.list[i].y] > 0) {
+          console.log("Eating grass...");
+          mat[this.list[i].x][this.list[i].y] = mat[this.list[i].x][this.list[i].y] - 0.1;
+        } else {
+          var superior = {x: -1, y: -1, value: 0};
+          for (var a = -1; a <= 1; a++) {
+            for (var b = -1; b <= 1; b++) {
+              var x = this.list[i].x + a;
+              var y = this.list[i].y + b;
+              console.log("Looking for a better place... x: " + x + " y: " + y);
+              if (x >= 0 && x < 50 && y >= 0 && y < 50) {
+                if (mat[x][y] > superior.value) {
+                  superior.value = mat[x][y];
+                  superior.x = x;
+                  superior.y = y;
+                }
+              }
+            }
+          }
+
+          while(superior.x < 0 || superior.x > 49 || superior.y < 0 || superior.y > 49) {
+            superior.x = this.list[i].x + ([-1,0,1][Math.random()*3|0]);
+            superior.y = this.list[i].y + ([-1,0,1][Math.random()*3|0]);
+          }
+          this.list[i].x = superior.x;
+          this.list[i].y = superior.y;
+          console.log("Moving to x: " + superior.x + " y: " + superior.y);
+        }
+      }
+    }
 };
--- grass.js
+++ grass.js
@@ -1,28 +1,29 @@
 var grass = {
+    height: 50,
+    width: 50,
     mat: [],
     init: function(ctx) {
       noise.seed(Math.random());
       console.log("Init grass");
-      for (var i = 0; i < 50; i++) {
-        for (var j = 0; j < 50; j++) {
+      for (var i = 0; i < this.width; i++) {
+        for (var j = 0; j < this.height; j++) {
           if (!this.mat[i]) this.mat[i] = [];
           var grassSize = Math.abs(noise.perlin2(i / 20, j / 20));//Math.floor((Math.random() * 10) + 1);
-          console.log("mat[" + i + "][" + j + "]: " + grassSize);
+          //console.log("mat[" + i + "][" + j + "]: " + grassSize);
           this.mat[i][j] = grassSize.toFixed(1);
         }
       }
     },
     draw: function(ctx) {
-
-      //ctx.fillStyle = "rgb(255, 255, 255)";
-      //ctx.fillRect(0, 0, 500, 500);
-      for (var i = 0; i < 50; i++) {
-        for (var j = 0; j < 50; j++) {
-          ctx.fillStyle = "rgba(0, 100, 0, "+(this.mat[i][j] * 1)+")";
+      ctx.fillStyle = "rgb(255, 255, 255)";
+      ctx.fillRect(0, 0, 500, 500);
+      for (var i = 0; i < this.width; i++) {
+        for (var j = 0; j < this.height; j++) {
+          ctx.fillStyle = "rgba(0, 100, 0, " + this.mat[i][j] + ")";
           var size = 10;
           var x = i * size;
           var y = j * size;
-          console.log("x: " + x + " y: " + y + " = " + (this.mat[i][j] * 1));
+          //console.log("x: " + x + " y: " + y + " = " + this.mat[i][j]);
           ctx.fillRect(x, y, size, size);
         }
       }
